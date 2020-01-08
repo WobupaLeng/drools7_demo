4a7db859b6fdf5d34c4a77ef7def132f79ec6dd4
edgar <electric.qiu@gmail.com>
2020-01-08 17:23:43 +0800

    update

--- src/main/java/constant/CommonDose.java
+++ src/main/java/constant/CommonDose.java
@@ -3,17 +3,6 @@ package constant;
 import common.SingleIntEnum;
 
 public enum CommonDose implements SingleIntEnum {
-    /*1. 低于每次推荐剂量/低于每日推荐剂量/低于推荐频次/低于推荐用药天数：慎用。
-      2. 高于每次推荐剂量/高于每日推荐剂量/高于推荐频次/高于推荐用药天数：不推荐。
-
-      3. 高于每次限制量/高于每日限制量：禁用。
-      4. 高于最大用药天数：禁用。
-      5. 高于疗程总剂量：禁用。
-
-      单日 = 每次 * 频次
-      总量 = 单日 * 天数
-    */
-
     EACH(1, "每次剂量")
     , DAILY(2, "每日剂量")
     , FREQUENCY(3, "频次")
--- src/main/java/constant/Concentration.java
+++ src/main/java/constant/Concentration.java
@@ -3,15 +3,6 @@ package constant;
 import common.SingleIntEnum;
 
 public enum Concentration implements SingleIntEnum {
-    /*
-    配伍浓度自定义审查维护的是药品与溶媒的适宜浓度范围或溶媒的适宜用量。
-    当处方满足所设置的审查条件（包括给药途径、给药单位），并超出相应的浓度范围或溶媒用量时，系统将予以警示。
-
-    1. 高于限制浓度/低于限制溶媒量：禁用。
-    2. 高于推荐浓度/高于限制溶媒量/低于限制浓度/低于推荐溶媒量：不推荐。
-    3. 低于推荐浓度/高于推荐溶媒量：慎用。
-    */
-
     DRUG(1, "药品")
     , SOLVENT(2, "溶媒")
     ;
